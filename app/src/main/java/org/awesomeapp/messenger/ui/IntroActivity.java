82b90992ad455b5e76f9aa956e410f7cd2c5c42a
KhanhTD <kasuko.go@gmail.com>
2017-11-13 15:32:43 +0700

    add new activity

---
+++ app/src/main/java/org/awesomeapp/messenger/ui/IntroActivity.java
@@ -0,0 +1,9 @@
+package org.awesomeapp.messenger.ui;
+
+/**
+ * Created by PCPV on 11/08/2017.
+ */
+
+public class IntroActivity extends BaseActivity {
+
+}
---
+++ app/src/main/java/org/awesomeapp/messenger/ui/LauncherActivity.java
@@ -0,0 +1,17 @@
+package org.awesomeapp.messenger.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import im.zom.messenger.R;
+
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+}
