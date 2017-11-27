c2478c916bf29d264deb9a972852ba3871b0b471
KhoaNguyen.PRD <khoand@proteusion.com>
2017-11-27 17:31:52 +0700

    - Update conference modes Video/Audio - Update gradle to fix crash on some of android devices has 64-bit processors(multi-dex issues).

--- app/src/main/java/net/wrappy/im/ui/ConferenceActivity.java
+++ app/src/main/java/net/wrappy/im/ui/ConferenceActivity.java
@@ -1,114 +1,88 @@
 package net.wrappy.im.ui;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
+import net.wrappy.im.BuildConfig;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetView;
-import org.jitsi.meet.sdk.JitsiMeetViewAdapter;
+import org.jitsi.meet.sdk.JitsiMeetViewListener;
 
 import java.util.Map;
 
-public class ConferenceActivity extends BaseActivity {
-
-    private JitsiMeetView view;
+public class ConferenceActivity extends JitsiMeetActivity {
+    JitsiMeetView view;
 
     @Override
-    public void onBackPressed() {
-        if (!view.onBackPressed()) {
-            // Invoke the default handler if it wasn't handled by React.
-            super.onBackPressed();
-        }
-    }
+    protected JitsiMeetView initializeView() {
+        view = super.initializeView();
 
+        // XXX In order to increase (1) awareness of API breakages and (2) API
+        // coverage, utilize JitsiMeetViewListener in the Debug configuration of
+        // the app.
+        if (BuildConfig.DEBUG && view != null) {
+            view.setListener(new JitsiMeetViewListener() {
+                private void on(String name, Map<String, Object> data) {
+                    // Log with the tag "ReactNative" in order to have the log
+                    // visible in react-native log-android as well.
+                    Log.d(
+                            "ReactNative",
+                            JitsiMeetViewListener.class.getSimpleName() + " "
+                                    + name + " "
+                                    + data);
+                }
+
+                @Override
+                public void onConferenceFailed(Map<String, Object> data) {
+                    on("CONFERENCE_FAILED", data);
+                }
+
+                @Override
+                public void onConferenceJoined(Map<String, Object> data) {
+                    on("CONFERENCE_JOINED", data);
+                }
+
+                @Override
+                public void onConferenceLeft(Map<String, Object> data) {
+                    on("CONFERENCE_LEFT", data);
+                }
+
+                @Override
+                public void onConferenceWillJoin(Map<String, Object> data) {
+                    on("CONFERENCE_WILL_JOIN", data);
+                }
+
+                @Override
+                public void onConferenceWillLeave(Map<String, Object> data) {
+                    on("CONFERENCE_WILL_LEAVE", data);
+                }
+
+                @Override
+                public void onLoadConfigError(Map<String, Object> data) {
+                    on("LOAD_CONFIG_ERROR", data);
+                }
+            });
+        }
+        return view;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // As this is the Jitsi Meet app (i.e. not the Jitsi Meet SDK), we do
+        // want the Welcome page to be enabled. It defaults to disabled in the
+        // SDK at the time of this writing but it is clearer to be explicit
+        // about what we want anyway.
+        setWelcomePageEnabled(false);
         super.onCreate(savedInstanceState);
-        view = new JitsiMeetView(this);
-        view.setListener(new JitsiMeetViewAdapter() {
-            @Override
-            public void onConferenceFailed(Map<String, Object> data) {
-                super.onConferenceFailed(data);
-                log("onConferenceFailed");
-            }
-
-            private void log(String log) {
-                Log.d("tag", log);
-            }
-
-            @Override
-            public void onConferenceJoined(Map<String, Object> data) {
-                super.onConferenceJoined(data);
-                log("onConferenceJoined");
-            }
-
-            @Override
-            public void onConferenceLeft(Map<String, Object> data) {
-                super.onConferenceLeft(data);
-                log("onConferenceLeft");
-            }
-
-            @Override
-            public void onConferenceWillJoin(Map<String, Object> data) {
-                super.onConferenceWillJoin(data);
-                log("onConferenceWillJoin");
-            }
-
-            @Override
-            public void onConferenceWillLeave(Map<String, Object> data) {
-                super.onConferenceWillLeave(data);
-                log("onConferenceWillLeave");
-            }
-
-            @Override
-            public void onLoadConfigError(Map<String, Object> data) {
-                super.onLoadConfigError(data);
-                log("onLoadConfigError");
-            }
-        });
         Bundle config = new Bundle();
-        config.putBoolean("startWithAudioMuted", true);
-        config.putBoolean("startWithVideoMuted", false);
+        config.putBoolean("startWithAudioMuted", false);
+        config.putBoolean("startWithVideoMuted", true);
         Bundle urlObject = new Bundle();
         urlObject.putBundle("config", config);
-
-        urlObject.putString("url", "https://meet.jit.si/tata2");
-//        view.setWelcomePageEnabled(true);
-        view.setBackgroundColor(Color.WHITE);
-        view.loadURLObject(config);
-
-        setContentView(view);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        view.dispose();
-        view = null;
-
-        JitsiMeetView.onHostDestroy(this);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        if (view != null)
-            view.onNewIntent(intent);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        view.onHostPause(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        view.onHostResume(this);
+        urlObject.putString("url", "https://meet.jit.si/Test123");
+        if (view != null) {
+            view.loadURLObject(urlObject);
+        }
     }
 }
