8398d0ce703de4c533ee146acab2270f1c20e65f
KhoaNguyen.PRD <khoand@proteusion.com>
2017-12-06 14:44:11 +0700

    - Update Account fields in db

---
+++ app/src/main/java/net/wrappy/im/model/RegistrationAccount.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2007 Esmertec AG. Copyright (C) 2007 The Android Open Source
+ * Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package net.wrappy.im.model;
+
+import net.wrappy.im.plugin.xmpp.XmppAddress;
+
+public class RegistrationAccount {
+    private String username;
+    private String jid;
+    private String nickname;
+    private String domain;
+    private String password;
+    private String server;
+    private int port;
+    private String email;
+    private String phone;
+    private String gender;
+
+    public RegistrationAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+        XmppAddress address = new XmppAddress(getUsername());
+        this.jid = address.getUser();
+        this.domain = address.getAddress().split("@")[1];
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getJid() {
+        return jid;
+    }
+}
