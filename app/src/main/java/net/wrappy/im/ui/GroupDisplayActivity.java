824526e55689f8169c4a33090fe51c4e53f33790
Tung Nguyen <tungnp@proteusion.com>
2017-12-11 11:57:26 +0700

    avoid crash

--- app/src/main/java/net/wrappy/im/ui/GroupDisplayActivity.java
+++ app/src/main/java/net/wrappy/im/ui/GroupDisplayActivity.java
@@ -27,9 +27,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.apache.commons.codec.DecoderException;
 import net.wrappy.im.ImApp;
 import net.wrappy.im.MainActivity;
+import net.wrappy.im.R;
 import net.wrappy.im.model.Contact;
 import net.wrappy.im.plugin.xmpp.XmppAddress;
 import net.wrappy.im.provider.Imps;
@@ -45,9 +45,9 @@ import net.wrappy.im.ui.qr.QrShareAsyncTask;
 import net.wrappy.im.ui.widgets.GroupAvatar;
 import net.wrappy.im.ui.widgets.LetterAvatar;
 
-import java.util.ArrayList;
+import org.apache.commons.codec.DecoderException;
 
-import net.wrappy.im.R;
+import java.util.ArrayList;
 
 public class GroupDisplayActivity extends BaseActivity {
 
@@ -566,7 +566,8 @@ public class GroupDisplayActivity extends BaseActivity {
 
     public void setMuted(boolean muted) {
         try {
-            mSession.setMuted(muted);
+            if (mSession != null)
+                mSession.setMuted(muted);
         }
         catch (RemoteException re){}
     }
