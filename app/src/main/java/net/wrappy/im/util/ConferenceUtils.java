87f9e9a4313bda92977a1cacc4ce1088d930b3e8
cuongduong <cuongdnq@proteusion.com>
2017-12-14 15:50:15 +0700

    adding saveBitmapPreferences

--- app/src/main/java/net/wrappy/im/util/ConferenceUtils.java
+++ app/src/main/java/net/wrappy/im/util/ConferenceUtils.java
@@ -1,6 +1,9 @@
 package net.wrappy.im.util;
 
 
+import android.content.Context;
+import android.net.Uri;
+
 import net.wrappy.im.model.ConferenceMessage;
 import net.wrappy.im.ui.conference.ConferenceConstant;
 
@@ -39,4 +42,8 @@ public final class ConferenceUtils {
     public static boolean isInvisibleMessage(String message) {
         return message.startsWith(ConferenceConstant.SEND_BACKGROUND_CHAT_PREFIX) || message.startsWith(ConferenceConstant.DELETE_CHAT_FREFIX) || message.startsWith(ConferenceConstant.EDIT_CHAT_FREFIX);
 	}
+
+    public static void saveBitmapPreferences(Uri imagePath, String mNickname, Context context) {
+        PreferenceUtils.putString(mNickname, imagePath.toString(), context);
+    }
 }
