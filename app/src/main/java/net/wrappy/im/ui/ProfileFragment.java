1468de8a7013f1799ac0c7df83fba766cc2ae1e5
KhoaNguyen.PRD <khoand@proteusion.com>
2017-12-22 10:00:35 +0700

    - update missing import

--- app/src/main/java/net/wrappy/im/ui/ProfileFragment.java
+++ app/src/main/java/net/wrappy/im/ui/ProfileFragment.java
@@ -1,6 +1,7 @@
 package net.wrappy.im.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialog;
