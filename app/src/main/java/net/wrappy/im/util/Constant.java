826ada3095ee52ad66e79426ef5cd5c7d8299761
KhoaNguyen.PRD <khoand@proteusion.com>
2018-02-01 13:24:47 +0700

    - Hide conference call

--- app/src/main/java/net/wrappy/im/util/Constant.java
+++ app/src/main/java/net/wrappy/im/util/Constant.java
@@ -9,6 +9,7 @@ public class Constant {
     public final static String EMAIL_DOMAIN = "@" + DOMAIN;
     public final static String DEFAULT_CONFERENCE_SERVER = "conference." + DOMAIN;
     public final static boolean OMEMO_ENABLED = false;
+    public final static boolean CONFERENCE_ENABLED = false;
     public final static long TIME_DELETE_MESSAGE = 30;//Days to delete message in local database
     public static final long MISSED_CALL_TIME = 60000L;//Time to set the call was missed
 }
