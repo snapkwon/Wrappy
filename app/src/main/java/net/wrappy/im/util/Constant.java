49c3847c789babd689e18be1fc321cd8632d6b6c
Tung Nguyen <tungnp@proteusion.com>
2018-02-03 15:29:44 +0700

    added dev flavor

--- app/src/main/java/net/wrappy/im/util/Constant.java
+++ app/src/main/java/net/wrappy/im/util/Constant.java
@@ -1,11 +1,13 @@
 package net.wrappy.im.util;
 
+import net.wrappy.im.BuildConfig;
+
 /**
  * Created by hp on 12/1/2017.
  */
 
 public class Constant {
-    public final static String DOMAIN = "im.wrappy.network";
+    public final static String DOMAIN = BuildConfig.DOMAIN;
     public final static String EMAIL_DOMAIN = "@" + DOMAIN;
     public final static String DEFAULT_CONFERENCE_SERVER = "conference." + DOMAIN;
     public final static boolean OMEMO_ENABLED = false;
