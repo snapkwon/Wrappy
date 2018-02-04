491302e03375d24112d6432bcf7b04464b44ac7b
cuongquoc2710 <cuongdnq@proteusion.com>
2018-02-04 09:57:49 +0700

    show or hide password in edit text

--- app/src/main/java/net/wrappy/im/ui/BaseActivity.java
+++ app/src/main/java/net/wrappy/im/ui/BaseActivity.java
@@ -9,9 +9,12 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -166,6 +169,37 @@ public class BaseActivity extends AppCompatActivity {
             toolbar.setTitleTextColor(themeColorText);
         }
 
+    }
+
+    public void showHidePassword(final EditText editText) {
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            boolean isVisible = false;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+
+                    if (motionEvent.getRawX() >= (editText.getRight() - editText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        if (!isVisible) {
+                            editText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                            editText.setSelection(editText.length());
+                            isVisible = true;
+                        } else {
+                            editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            editText.setSelection(editText.length());
+                            isVisible = false;
+                        }
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 
