7b527a2e3c3b1c5ffa0be3186d94965ff055efb0
Hienlt <hiendeveloper505@gmail.com>
2018-02-08 12:29:26 +0700

    Merge branch 'dev' of https://github.com/prdlab/2017-net.wrappy.im-fe-android into dev_barged
    # Conflicts:
    #	app/src/main/java/net/wrappy/im/MainActivity.java
    #	app/src/main/java/net/wrappy/im/helper/AppFuncs.java

--- app/src/main/java/net/wrappy/im/ui/LauncherActivity.java
+++ app/src/main/java/net/wrappy/im/ui/LauncherActivity.java
@@ -2,35 +2,33 @@ package net.wrappy.im.ui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
+import net.wrappy.im.BuildConfig;
 import net.wrappy.im.R;
 import net.wrappy.im.helper.AppFuncs;
+import net.wrappy.im.helper.layout.AppTextView;
 import net.wrappy.im.provider.Store;
 import net.wrappy.im.util.PopupUtils;
+import net.wrappy.im.util.Utils;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
 public class LauncherActivity extends BaseActivity {
 
-    private ViewFlipper mViewFlipper;
-    private EditText mEditUsername;
-    private Button mBtnLogin;
-    private Button mBtnregister;
-    LinearLayout lnLoginFrame;
+    @BindView(R.id.viewFlipper1) ViewFlipper mViewFlipper;
+    @BindView(R.id.edtUserMame) EditText mEditUsername;
+    @BindView(R.id.txtLoginVersionName) AppTextView txtLoginVersionName;
+
     public static final int REQUEST_CODE_REGISTER = 1111;
     public static final int REQUEST_CODE_LOGIN = 1112;
     public static final int REQUEST_CODE_INPUT_NEW_PASSWORD = 1113;
-
     public static final int RESULT_ERROR= 4001;
 
     boolean isFlag;
@@ -43,91 +41,14 @@ public class LauncherActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        getSupportActionBar().setTitle("");
-
-        View viewSplash = findViewById(R.id.flipViewMain);
-
-        mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
-
-        getSupportActionBar().hide();
-        getSupportActionBar().setTitle("");
-
-        mViewFlipper.setDisplayedChild(0);
-        lnLoginFrame = (LinearLayout) viewSplash.findViewById(R.id.lnLoginFrame);
-        mEditUsername = (EditText) viewSplash.findViewById(R.id.edtUserMame);
-        mBtnLogin = (Button) viewSplash.findViewById(R.id.btnShowLogin);
-        mBtnregister = (Button) viewSplash.findViewById(R.id.btnShowRegister);
-
-        mBtnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(mEditUsername.getText().toString().trim())) {
-                    PopupUtils.showCustomDialog(LauncherActivity.this, getString(R.string.warning), getString(R.string.error_empty_username)
-                            , R.string.yes, null, false);
-                } else {
-                    Store.putStringData(getApplicationContext(), Store.USERNAME, mEditUsername.getText().toString().trim());
-                    showLogin();
-                }
-            }
-        });
-
-        mBtnregister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showRegister();
-            }
-        });
-
-        lnLoginFrame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isFlag) {
-                    return;
-                } isFlag = true;
-                AppFuncs.dismissKeyboard(LauncherActivity.this);
-                isFlag = false;
-            }
-        });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case android.R.id.home:
-
-                showPrevious();
-
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void showPrevious() {
-        setAnimRight();
-        getSupportActionBar().setTitle("");
-
-        if (mViewFlipper.getCurrentView().getId() == R.id.flipViewMain) {
-            finish();
-        } else if (mViewFlipper.getCurrentView().getId() == R.id.flipViewLogin
-                || mViewFlipper.getCurrentView().getId() == R.id.flipViewRegister) {
-            showSplash();
-        }
-
-    }
-
-    private void showSplash() {
+        super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         mViewFlipper.setDisplayedChild(0);
 
+        String versionName = BuildConfig.VERSION_NAME;
+        Utils.setTextForView(txtLoginVersionName,versionName);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
@@ -138,6 +59,29 @@ public class LauncherActivity extends BaseActivity {
                    }
                     break;
             }
+    }
+
+    @OnClick({R.id.btnShowLogin,R.id.btnShowRegister,R.id.lnLoginFrame})
+    public void onClick(View v) {
+        if (isFlag) {
+            return;
+        } isFlag = true;
+        AppFuncs.dismissKeyboard(LauncherActivity.this);
+        switch (v.getId()) {
+            case R.id.btnShowLogin:
+                if (TextUtils.isEmpty(mEditUsername.getText().toString().trim())) {
+                    PopupUtils.showCustomDialog(LauncherActivity.this, getString(R.string.warning), getString(R.string.error_empty_username)
+                            , R.string.yes, null, false);
+                } else {
+                    Store.putStringData(getApplicationContext(), Store.USERNAME, mEditUsername.getText().toString().trim());
+                    showLogin();
+                }
+                break;
+            case R.id.btnShowRegister:
+                showRegister();
+                break;
+        }
+        isFlag = false;
     }
 
 
@@ -159,42 +103,5 @@ public class LauncherActivity extends BaseActivity {
         arg.putString("username", "");
         intent.putExtras(arg);
         startActivity(intent);
-    }
-
-   /* @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_CODE_REGISTER) {
-                String result = data.getStringExtra("result");
-
-                JsonArray jsonArray = new JsonArray();
-
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("UserName",mEditUsername.getText().toString());
-                jsonObject.addProperty("PassWord",result);
-
-
-                jsonArray.add(jsonObject);
-                new RestAPI.PostDataUrl(jsonArray.toString(), this).execute(RestAPI.POST_REGISTER);
-            } else  if (requestCode == REQUEST_CODE_LOGIN) {
-                String result = data.getStringExtra("result");
-            }
-        }
-    }*/
-
-
-    private void setAnimLeft() {
-        Animation animIn = AnimationUtils.loadAnimation(this, R.anim.push_left_in);
-        Animation animOut = AnimationUtils.loadAnimation(this, R.anim.push_left_out);
-        mViewFlipper.setInAnimation(animIn);
-        mViewFlipper.setOutAnimation(animOut);
-    }
-
-    private void setAnimRight() {
-        Animation animIn = AnimationUtils.loadAnimation(LauncherActivity.this, R.anim.push_right_in);
-        Animation animOut = AnimationUtils.loadAnimation(LauncherActivity.this, R.anim.push_right_out);
-        mViewFlipper.setInAnimation(animIn);
-        mViewFlipper.setOutAnimation(animOut);
     }
 }
